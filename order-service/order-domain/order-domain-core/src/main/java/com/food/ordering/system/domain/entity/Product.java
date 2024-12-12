934bdcde60a8fb3aa0abc944896f5301b05fac90
Aabilla Said <said160aabilla@gmail.com>
2024-12-12 17:23:12 +0100

    created entities and vale objects in order-domain-core

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/domain/entity/Product.java
@@ -0,0 +1,23 @@
+package com.food.ordering.system.domain.entity;
+
+import com.food.ordering.system.domain.valueobject.Money;
+import com.food.ordering.system.domain.valueobject.ProductId;
+
+public class Product extends BaseEntity<ProductId> {
+    private String name;
+    private Money price;
+
+    public Product(String name, Money price) {
+        this.name = name;
+        this.price = price;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+}
