d1a15e657be598b2cb51f14af835ba4d31c5b132
Aabilla Said <said160aabilla@gmail.com>
2024-12-13 16:04:44 +0100

    added core domain and common logic

--- common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/OrderId.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/OrderId.java
@@ -3,7 +3,7 @@ package com.food.ordering.system.domain.valueobject;
 import java.util.Objects;
 import java.util.UUID;
 
-public abstract class OrderId  extends BaseId<UUID> {
+public class OrderId  extends BaseId<UUID> {
 
     public OrderId(UUID value) {
         super(value);
