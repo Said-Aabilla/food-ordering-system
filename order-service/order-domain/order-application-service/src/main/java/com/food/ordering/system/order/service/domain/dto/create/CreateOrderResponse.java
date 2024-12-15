467a7f7353f2d9a31b2ab1f0ac04ae5b2e1eacb0
Aabilla Said <said160aabilla@gmail.com>
2024-12-15 21:25:35 +0100

    added dto classes: create{order}, message {payment status,restaurant approval} track {order}

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/create/CreateOrderResponse.java
@@ -0,0 +1,31 @@
+package com.food.ordering.system.order.service.domain.dto.create;
+
+
+import com.food.ordering.system.domain.valueobject.OrderStatus;
+import com.food.ordering.system.domain.valueobject.TrackingId;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@AllArgsConstructor
+@Getter
+@Builder
+public class CreateOrderResponse {
+
+
+    @NotNull
+    public final UUID trackingId;
+
+    @NotNull
+    public final OrderStatus orderStatus;
+
+    @NotNull
+    public final BigDecimal price;
+
+    @NotNull
+    public String message;
+
+}
