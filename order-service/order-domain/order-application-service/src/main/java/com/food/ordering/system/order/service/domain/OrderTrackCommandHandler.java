3a47035612b60df95873c6fc941faae9f8a67c28
Aabilla Said <said160aabilla@gmail.com>
2024-12-17 21:08:50 +0100

    impl of OrderApplicationService: port definitions {order create}, {order track}

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
@@ -0,0 +1,17 @@
+package com.food.ordering.system.order.service.domain;
+
+
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
+import com.food.ordering.system.order.service.domain.dto.track.TrackingOrderResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+
+@Slf4j
+@Component
+public class OrderTrackCommandHandler {
+    TrackingOrderResponse trackOrder (TrackOrderQuery trackOrderQuery){
+
+    }
+
+}
