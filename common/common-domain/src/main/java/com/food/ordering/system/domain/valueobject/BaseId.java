e2879e131a5c279e3e6e936137d0cd9e4d7f3a78
Aabilla Said <said160aabilla@gmail.com>
2024-12-17 17:29:25 +0100

    added getValue() method

--- common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/BaseId.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/BaseId.java
@@ -3,25 +3,26 @@ package com.food.ordering.system.domain.valueobject;
 import java.util.Objects;
 
 public abstract class BaseId<T> {
-
     private final T value;
 
     protected BaseId(T value) {
         this.value = value;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        BaseId<?> that = (BaseId<?>) obj;
-        return value.equals(that.value);
+    public T getValue() {
+        return value;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseId<?> baseId = (BaseId<?>) o;
+        return value.equals(baseId.value);
+    }
 
     @Override
     public int hashCode() {
         return Objects.hash(value);
     }
-
 }
--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/create/CreateOrderCommand.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/create/CreateOrderCommand.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @Getter
 @Builder
-public class CreateOrderCommand {
+public class CreateOrderCommandDto {
 
 
     @NotNull
