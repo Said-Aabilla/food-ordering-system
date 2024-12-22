7683f49d559146f1475d68187b5990967da1716f
Aabilla Said <said160aabilla@gmail.com>
2024-12-22 22:55:29 +0100

    working on tests {OrderApplicationServiceTest} using mockito and spring boot starter test

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/domain/entity/Product.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/domain/entity/Product.java
@@ -16,6 +16,12 @@ public class Product extends BaseEntity<ProductId> {
         super.setId(productId);
     }
 
+    public Product(ProductId productId, String name, Money price) {
+        super.setId(productId);
+        this.name = name;
+        this.price = price;
+    }
+
     public void updateWithConfirmedNameAndPrice(String name, Money price) {
         this.name = name;
         this.price = price;
