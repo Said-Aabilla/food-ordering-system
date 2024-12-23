49260b38fcb468492d12a2f878063987461d0ae1
Aabilla Said <said160aabilla@gmail.com>
2024-12-23 13:27:17 +0100

    fixed bug in method: validateItemPrice() in class {Order}

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/domain/entity/Order.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/domain/entity/Order.java
@@ -101,9 +101,9 @@ public class Order extends AggregateRoot<OrderId>{
     }
 
     private void validateItemPrice(OrderItem orderItem) {
-      if(orderItem.isPriceValid())
+      if(!orderItem.isPriceValid())
           throw new OrderDomainException("Order Item price: "+  orderItem.getPrice().getAmount()
-                  + " is not valid for product: " + orderItem.getProduct().getId().toString() + " !") ;
+                  + " is not valid for product: " + orderItem.getProduct().getId().getValue() + " !") ;
     }
 
 
