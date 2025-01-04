e2617621b52e0ddc8da232751a07451ed5654637
Aabilla Said <said160aabilla@gmail.com>
2025-01-04 12:24:22 +0100

    fixed typo in module names to fit the naming standard

--- infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/order/service/messaging/kafka/producer/exception/KafkaProducerException.java
+++
@@ -1,7 +0,0 @@
-package com.food.ordering.system.order.service.messaging.kafka.producer.exception;
-
-public class KafkaProducerException  extends RuntimeException{
-    public KafkaProducerException(String message) {
-        super(message);
-    }
-}
--- order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/listener/kafka/PaymentResponseKafkaListener.java
+++ order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/listener/kafka/PaymentResponseKafkaListener.java
@@ -4,7 +4,7 @@ package com.food.ordering.system.order.service.messaging.listener.kafka;
 import com.food.ordering.system.kafka.order.avro.model.PaymentResponseAvroModel;
 import com.food.ordering.system.kafka.order.avro.model.PaymentStatus;
 import com.food.ordering.system.order.service.domain.ports.input.message.listener.payment.PaymentResponseMessageListener;
-import com.food.ordering.system.order.service.messaging.kafka.consumer.KafkaConsumer;
+import com.food.ordering.system.kafka.consumer.KafkaConsumer;
 import com.food.ordering.system.order.service.messaging.mapper.OrderMessagingDataMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
