022974136e6a0b87c1584a8756ff33f2fe732339
Aabilla Said <said160aabilla@gmail.com>
2025-01-10 11:40:43 +0100

    used ZONE_ID from common domain constants

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/OrderDomainServiceImpl.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/OrderDomainServiceImpl.java
@@ -15,10 +15,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static com.food.ordering.system.domain.constant.DomainConstant.ZONE_ID;
+
 
 @Slf4j
 public class OrderDomainServiceImpl implements  OrderDomainService{
-    private static final String TIMEZONE = "GMT";
     @Override
     public OrderCreatedEvent validateAndInitiateOrder(Order order, Restaurant restaurant) {
         validateRestaurant(restaurant);
@@ -26,14 +27,14 @@ public class OrderDomainServiceImpl implements  OrderDomainService{
         order.validateOrder();
         order.initializeOrder();
         log.info("Order with id: {} is initiated", order.getId().getValue());
-        return new OrderCreatedEvent(order, ZonedDateTime.now(ZoneId.of(TIMEZONE)));
+        return new OrderCreatedEvent(order, ZonedDateTime.now(ZoneId.of(ZONE_ID)));
     }
 
     @Override
     public OrderPaidEvent payOrder(Order order) {
         order.pay();
         log.info("Order with id: {} is paid", order.getId().toString());
-        return new OrderPaidEvent(order, ZonedDateTime.now(ZoneId.of(TIMEZONE)));    }
+        return new OrderPaidEvent(order, ZonedDateTime.now(ZoneId.of(ZONE_ID)));    }
 
     @Override
     public void approveOrder(Order order) {
@@ -45,7 +46,7 @@ public class OrderDomainServiceImpl implements  OrderDomainService{
     public OrderCancelledEvent cancelOrderPayment(Order order, List<String> failureMessages) {
         order.initCancel(failureMessages);
         log.info("Order payment is cancelling for order with id: {} ", order.getId().toString());
-        return new OrderCancelledEvent(order, ZonedDateTime.now(ZoneId.of(TIMEZONE)));
+        return new OrderCancelledEvent(order, ZonedDateTime.now(ZoneId.of(ZONE_ID)));
     }
 
     @Override
