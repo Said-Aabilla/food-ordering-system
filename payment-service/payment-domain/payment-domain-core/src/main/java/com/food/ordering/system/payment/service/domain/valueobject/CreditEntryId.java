bb1b3e913cde86b4bd973cf0fcb0a860fd25dacb
Aabilla Said <said160aabilla@gmail.com>
2025-01-14 15:15:44 +0100

    Impl of output ports in payment service module by adapters in data access module

--- payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/service/domain/valueobject/CreditEntryId.java
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/service/domain/valueobject/CreditEntryId.java
@@ -1,11 +1,11 @@
 package com.food.ordering.system.payment.service.domain.valueobject;
 
-import com.food.ordering.system.order.service.domain.valueobject.BaseId;
+import com.food.ordering.system.domain.valueobject.BaseId;
 
 import java.util.UUID;
 
 public class CreditEntryId extends BaseId<UUID> {
-    protected CreditEntryId(UUID value) {
+    public CreditEntryId(UUID value) {
         super(value);
     }
 }
