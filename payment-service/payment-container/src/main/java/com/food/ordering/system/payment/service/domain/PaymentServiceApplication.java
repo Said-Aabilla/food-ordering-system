54053c77d7ab73016a8603fb066fa223af3d1676
Aabilla Said <said160aabilla@gmail.com>
2025-01-19 11:54:03 +0100

    rename dataaccess to data.access

--- payment-service/payment-container/src/main/java/com/food/ordering/system/payment/service/domain/PaymentServiceApplication.java
+++ payment-service/payment-container/src/main/java/com/food/ordering/system/payment/service/domain/PaymentServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = "com.food.ordering.system.payment.service.dataaccess")
-@EntityScan(basePackages = "com.food.ordering.system.payment.service.dataaccess")
+@EnableJpaRepositories(basePackages = "com.food.ordering.system.payment.service.data.access")
+@EntityScan(basePackages = "com.food.ordering.system.payment.service.data.access")
 @SpringBootApplication(scanBasePackages = "com.food.ordering.system")
 public class PaymentServiceApplication {
     public static void main(String[] args) {
