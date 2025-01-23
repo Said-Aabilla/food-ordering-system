6d02841152fd8b27602d361701209a4651ff9f84
Aabilla Said <said160aabilla@gmail.com>
2025-01-23 18:39:21 +0100

    created OrderSagaHelper and completed RestaurantApprovalResponseMessageListenerImpl with  OrderApprovalSaga process() and rollback()

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/saga/OrderPaymentSaga.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/saga/OrderPaymentSaga.java
@@ -1,37 +1,32 @@
-package com.food.ordering.system.order.service.domain;
+package com.food.ordering.system.order.service.domain.saga;
 
 import com.food.ordering.system.domain.event.EmptyEvent;
-import com.food.ordering.system.domain.exception.OrderNotFoundException;
-import com.food.ordering.system.domain.valueobject.OrderId;
+import com.food.ordering.system.order.service.domain.OrderDomainService;
 import com.food.ordering.system.order.service.domain.dto.message.PaymentResponse;
 import com.food.ordering.system.order.service.domain.entity.Order;
 import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
 import com.food.ordering.system.order.service.domain.ports.output.message.publisher.payment.OrderCancelledPaymentRequestMessagePublisher;
 import com.food.ordering.system.order.service.domain.ports.output.message.publisher.restaurantapproval.OrderPaidRestaurantRequestMessagePublisher;
-import com.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository;
 import com.food.ordering.system.saga.SagaStep;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-import java.util.UUID;
-
 @Slf4j
 @Component
 public class OrderPaymentSaga implements SagaStep<PaymentResponse, OrderPaidEvent, EmptyEvent> {
 
-    public final OrderDomainService orderDomainService;
-    public final OrderRepository orderRepository;
-    public final OrderCancelledPaymentRequestMessagePublisher orderCancelledPaymentRequestMessagePublisher;
-    public final OrderPaidRestaurantRequestMessagePublisher orderPaidRestaurantRequestMessagePublisher;
+    private final OrderDomainService orderDomainService;
+    private final OrderSagaHelper orderSagaHelper;
+    private final OrderCancelledPaymentRequestMessagePublisher orderCancelledPaymentRequestMessagePublisher;
+    private final OrderPaidRestaurantRequestMessagePublisher orderPaidRestaurantRequestMessagePublisher;
 
     public OrderPaymentSaga(OrderDomainService orderDomainService,
-                            OrderRepository orderRepository,
+                            OrderSagaHelper orderSagaHelper,
                             OrderCancelledPaymentRequestMessagePublisher orderCancelledPaymentRequestMessagePublisher,
                             OrderPaidRestaurantRequestMessagePublisher orderPaidRestaurantRequestMessagePublisher) {
         this.orderDomainService = orderDomainService;
-        this.orderRepository = orderRepository;
+        this.orderSagaHelper = orderSagaHelper;
         this.orderCancelledPaymentRequestMessagePublisher = orderCancelledPaymentRequestMessagePublisher;
         this.orderPaidRestaurantRequestMessagePublisher = orderPaidRestaurantRequestMessagePublisher;
     }
@@ -40,9 +35,9 @@ public class OrderPaymentSaga implements SagaStep<PaymentResponse, OrderPaidEven
     @Transactional
     public OrderPaidEvent process(PaymentResponse paymentResponse) {
         log.info("Completing Payment for Order with id {}", paymentResponse.getOrderId());
-        Order order = findOrder(paymentResponse.getOrderId());
+        Order order = orderSagaHelper.findOrder(paymentResponse.getOrderId());
         OrderPaidEvent orderPaidEvent = orderDomainService.payOrder(order,orderPaidRestaurantRequestMessagePublisher);
-        orderRepository.save(order);
+        orderSagaHelper.saveOrder(order);
         log.info("Payment Completed for Order with id {}", order.getId().getValue());
         return orderPaidEvent;
     }
@@ -51,19 +46,10 @@ public class OrderPaymentSaga implements SagaStep<PaymentResponse, OrderPaidEven
     @Transactional
     public EmptyEvent rollback(PaymentResponse paymentResponse) {
         log.info("Cancelling Order with id {}", paymentResponse.getOrderId());
-        Order order = findOrder(paymentResponse.getOrderId());
+        Order order = orderSagaHelper.findOrder(paymentResponse.getOrderId());
         orderDomainService.cancelOrder(order, paymentResponse.getFailureMessages());
-        orderRepository.save(order);
+        orderSagaHelper.saveOrder(order);
         log.info("Order with id {} has been cancelled", order.getId().getValue());
         return EmptyEvent.INSTANCE;
-    }
-
-    private Order findOrder(String orderId) {
-        Optional<Order> orderOptional= orderRepository.findById(new OrderId(UUID.fromString(orderId)));
-        if (orderOptional.isEmpty()){
-            log.error("Order with id {} could not be found",orderId);
-            throw new OrderNotFoundException("Order not found with id: "+orderId);
-        }
-        return orderOptional.get();
     }
 }
